1b0c7f9b7e1f030407f18d4ca3b0efb3d112ef4f
annabanananana <123480477+annabanananana@users.noreply.github.com>
2024-10-13 15:29:05 +0200

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,6 @@
 // initializes an SQLite database
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:');
+const db = new sqlite3.Database('./recipes.db');
 
 db.serialize(() => {
   db.run("CREATE TABLE recipes (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, ingredients TEXT, instructions TEXT)");
